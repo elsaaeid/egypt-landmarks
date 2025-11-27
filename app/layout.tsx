c869ebdb3068738b855f032316ee1ebb5be4582f
elsaaeid <92111076+elsaaeid@users.noreply.github.com>
2025-11-27 20:27:22 +0200

    deploy changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import './globals.css';
 import { Geist, Geist_Mono } from "next/font/google";
 import ScrollToTop from "../components/ScrollToTop";
+import CookieBanner from "../components/CookieBanner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -36,6 +37,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <CookieBanner />
         {children}
         <ScrollToTop />
       </body>
