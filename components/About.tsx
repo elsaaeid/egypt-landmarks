f3d6b202ec52f7c053c83cd27bf55b42e4d89181
elsaaeid <92111076+elsaaeid@users.noreply.github.com>
2026-07-23 23:18:24 +0200

    deploy changes

--- components/About.tsx
+++ components/About.tsx
@@ -9,10 +9,10 @@ const About: React.FC = () => {
         <div className="text-center max-w-2xl mx-auto">
           <p className="text-lg text-gray-700 mb-4 direction-rtl" dir="rtl">
             مصر هي مهد الحضارة، حيث يعود تاريخها إلى آلاف السنين. من النيل إلى الصحراء، تروي مصر قصصاً عن الفراعنة والآلهة والإنجازات 
-            .العلمية
+            العلمية
           </p>
           <p className="text-lg text-gray-700">
-            اكتشف الكنوز الأثرية التي جعلت مصر وجهة سياحية عالمية.
+            اكتشف الكنوز الأثرية التي جعلت مصر وجهة سياحية عالمية
           </p>
         </div>
       </div>
