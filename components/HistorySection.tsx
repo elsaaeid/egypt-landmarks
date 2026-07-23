02360ef7ded68bb6488b0f2917a49e44ee4994d4
elsaaeid <92111076+elsaaeid@users.noreply.github.com>
2026-07-23 23:47:03 +0200

    deploy changes

--- components/HistorySection.tsx
+++ components/HistorySection.tsx
@@ -10,13 +10,13 @@ const HistorySection: React.FC = () => {
           <div className='flex flex-col justify-center items-center'>
             <h3 className="text-2xl font-semibold mb-4">العصر القديم</h3>
             <p className="text-gray-700 text-center">
-              بدأت الحضارة المصرية القديمة حوالي 3100 قبل الميلاد مع توحيد المملكة العليا والسفلى. شهدت بناء الأهرامات والمعابد الضخمة.
+              .بدأت الحضارة المصرية القديمة حوالي 3100 قبل الميلاد مع توحيد المملكة العليا والسفلى. شهدت بناء الأهرامات والمعابد الضخمة
             </p>
           </div>
           <div className='flex flex-col justify-center items-center'>
             <h3 className="text-2xl font-semibold mb-4">العصر الحديث</h3>
             <p className="text-gray-700 text-center">
-              اليوم، تستمر مصر في الحفاظ على تراثها الغني بينما تتطور كدولة حديثة، مع جذب ملايين الزوار سنويا.
+              .اليوم، تستمر مصر في الحفاظ على تراثها الغني بينما تتطور كدولة حديثة، مع جذب ملايين الزوار سنويا
             </p>
           </div>
         </div>
